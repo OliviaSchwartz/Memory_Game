17a477aa2e302a330d0de34fd7b8ec83d90accf8
Olivia Schwartz <ocmooney@gmail.com>
2022-10-06 09:43:35 -0400

    Redoing Match Function

--- objects.js
+++ objects.js
@@ -1,11 +1,13 @@
 const resetButton = document.querySelector('.reset')
-const isGameActive = true
-const card = document.querySelectorAll('.card')
-const gameChoice = document.querySelectorAll('img')
+// const card = document.querySelectorAll('.card')
 const frontOfCard = document.querySelectorAll('.front')
 const backOfCard = document.querySelectorAll('.back')
 const playingArea = document.querySelectorAll('.play-area')
-let match
+const firstClick = ''
+const secondClick = ''
+const card = document.querySelectorAll('.card')
+const counter = 1
+const wrong = false
 const cardOne = (document.getElementById('cardset-one').dataset.value = 0)
 const cardTwo = (document.getElementById('cardset-two').dataset.value = 0)
 const cardThree = (document.getElementById('cardset-three').dataset.value = 1)
@@ -16,53 +18,22 @@ const cardSeven = (document.getElementById('cardset-seven').dataset.value = 3)
 const cardEight = (document.getElementById('cardset-eight').dataset.value = 3)
 const cardNine = (document.getElementById('cardset-nine').dataset.value = 4)
 const cardTen = (document.getElementById('cardset-ten').dataset.value = 4)
-const cardEleven = (document.getElementById(
-  'cardset-eleven'
-).dataset.value = 10)
-const cardTwelve = (document.getElementById(
-  'cardset-twelve'
-).dataset.value = 11)
-// const cardThirteen = (document.getElementById(
-//   'cardset-thirteen'
-// ).dataset.value = 12)
-// const cardFourteen = (document.getElementById(
-//   'cardset-fourteen'
-// ).dataset.value = 13)
-// const cardFifteen = (document.getElementById(
-//   'cardset-fifteen'
-// ).dataset.value = 14)
-// const cardSixteen = (document.getElementById(
-//   'cardset-sixteen'
-// ).dataset.value = 15)
-
-const matchCombinations = [
-  [cardOne, cardTwo],
-  [cardThree, cardFour],
-  [cardFive, cardSix],
-  [cardSeven, cardEight],
-  [cardNine, cardTen],
-  [cardEleven, cardTwelve]
-  //   [cardThirteen, cardFourteen],
-  //   [cardFifteen, cardSixteen]
+const cardEleven = (document.getElementById('cardset-eleven').dataset.value = 5)
+const cardTwelve = (document.getElementById('cardset-twelve').dataset.value = 5)
+const cardOptions = [
+  cardOne,
+  cardTwo,
+  cardThree,
+  cardFour,
+  cardFive,
+  cardSix,
+  cardSeven,
+  cardEight,
+  cardNine,
+  cardTen,
+  cardEleven,
+  cardTwelve
 ]
-const flipCard = (event) => {
-  event.target.classList.toggle('flip')
-  cardMatch()
-}
-
-let cardMatch = () => {
-  let match = false
-  let matchRules = matchCombinations.length
-  for (let i = 0; i < matchRules; i++) {
-    // const matchRules = matchCombinations[i]
-    let pickCardOne = matchCombinations[i][0]
-    let pickCardTwo = matchCombinations[i][1]
-
-    if (pickCardOne === pickCardTwo) {
-    }
-  }
-}
-cardMatch()
 const shuffle = () => {
   card.forEach((card) => {
     let shuffledCards = Math.floor(Math.random() * 16)
@@ -71,10 +42,71 @@ const shuffle = () => {
 }
 shuffle()
 
-// const flipCard = (event) => {
-//   event.target.classList.toggle('flip')
-// }
+const flipCard = (event) => {
+  event.target.classList.toggle('flip')
+}
 
+const selectFirstCard = () => {
+  firstClick.currentChoice = cardOptions[0]
+  //   compareCards()
+}
+
+document.querySelector('#cardset-one').addEventListener('click', flipCard)
 document
-  .querySelectorAll('.card')
-  .forEach((card) => card.addEventListener('click', flipCard))
+  .querySelector('#cardset-one')
+  .addEventListener('click', selectFirstCard)
+
+document.querySelector('#cardset-two').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-two')
+  .addEventListener('click', selectSecondCard)
+
+document.querySelector('#cardset-three').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-three')
+  .addEventListener('click', selectThirdCard)
+
+document.querySelector('#cardset-four').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-four')
+  .addEventListener('click', selectFourthCard)
+
+document.querySelector('#cardset-five').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-five')
+  .addEventListener('click', selectFifthCard)
+
+document.querySelector('#cardset-six').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-six')
+  .addEventListener('click', selectSixthCard)
+
+document.querySelector('#cardset-seven').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-seven')
+  .addEventListener('click', selectSeventhCard)
+
+document.querySelector('#cardset-eight').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-eight')
+  .addEventListener('click', selectEighthCard)
+
+document.querySelector('#cardset-nine').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-nine')
+  .addEventListener('click', selectNinthCard)
+
+document.querySelector('#cardset-ten').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-ten')
+  .addEventListener('click', selectTenthCard)
+
+document.querySelector('#cardset-eleven').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-eleven')
+  .addEventListener('click', selectEleventhCard)
+
+document.querySelector('#cardset-twelve').addEventListener('click', flipCard)
+document
+  .querySelector('#cardset-twelve')
+  .addEventListener('click', selectTwelfth)
